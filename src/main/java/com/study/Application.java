fa17ffe2ae9f9cdc30a02ba8b107edda62a2b5f1
sedovila <0554my@naver.com>
2022-01-10 03:03:16 +0900

    컨테이너와 포트

--- src/main/java/com/study/Application.java
+++ src/main/java/com/study/Application.java
@@ -1,18 +1,7 @@
 package com.study;
 
-import org.apache.catalina.Context;
-import org.apache.catalina.LifecycleException;
-import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 //@Configuration
 //@EnableAutoConfiguration
@@ -21,9 +10,9 @@ import java.io.PrintWriter;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) throws LifecycleException {
+    public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(Application.class);
-        springApplication.setWebApplicationType(WebApplicationType.NONE); //EnableAutoConfiguration이 없어도 실행됨.
+//        springApplication.setWebApplicationType(WebApplicationType.NONE); //EnableAutoConfiguration이 없어도 실행됨.
         springApplication.run(args);
 
     }
