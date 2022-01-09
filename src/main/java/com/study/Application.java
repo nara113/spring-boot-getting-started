c6f55f48dae9af86b6ab0694424fd2813bb61ae6
sedovila <0554my@naver.com>
2022-01-10 02:00:51 +0900

    tomcat

--- src/main/java/com/study/Application.java
+++ src/main/java/com/study/Application.java
@@ -1,12 +1,18 @@
 package com.study;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 //@Configuration
 //@EnableAutoConfiguration
@@ -15,10 +21,11 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 public class Application {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws LifecycleException {
         SpringApplication springApplication = new SpringApplication(Application.class);
         springApplication.setWebApplicationType(WebApplicationType.NONE); //EnableAutoConfiguration이 없어도 실행됨.
         springApplication.run(args);
+
     }
 
 //    @Bean
---
+++ src/main/java/com/study/Web.java
@@ -0,0 +1,38 @@
+package com.study;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Web {
+
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        Context context = tomcat.addContext("", "/");
+
+        HttpServlet httpServlet = new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                PrintWriter writer = resp.getWriter();
+                writer.println("hello !!");
+            }
+        };
+
+        String servletName = "helloServlet";
+        tomcat.addServlet("", servletName, httpServlet);
+        context.addServletMappingDecoded("/hello", servletName);
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+}
