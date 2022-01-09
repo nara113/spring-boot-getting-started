c2612627897af15a0f357e0beb9266a4743e906a
sedovila <0554my@naver.com>
2022-01-10 06:48:00 +0900

    SpringApplication

--- src/main/java/com/study/Application.java
+++ src/main/java/com/study/Application.java
@@ -1,24 +1,29 @@
 package com.study;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.Bean;
 
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication springApplication = new SpringApplication(Application.class);
-//        springApplication.setWebApplicationType(WebApplicationType.NONE); //EnableAutoConfiguration이 없어도 실행됨.
-        springApplication.run(args);
+        //커스터마이징 할 수 있다.
+        new SpringApplicationBuilder()
+                .sources(Application.class)
+                .bannerMode(Banner.Mode.OFF)
+                .run(args);
 
+//        SpringApplication springApplication = new SpringApplication(Application.class);
+//        springApplication.setWebApplicationType(WebApplicationType.NONE); //EnableAutoConfiguration이 없어도 실행됨.
+//        springApplication.run(args);
     }
 
-//    @Bean
-//    public Holoman holoman() {
-//        return new Holoman("new", 10);
-//    }
+    @Bean
+    public Holoman holoman() {
+        return new Holoman("new", 10);
+    }
 }
