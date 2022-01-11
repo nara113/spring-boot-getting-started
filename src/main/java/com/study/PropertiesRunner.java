773e49d068fd0b4a2bc6fe573f931ea526632f17
sedovila <0554my@naver.com>
2022-01-12 03:45:04 +0900

    로깅

--- src/main/java/com/study/PropertiesRunner.java
+++ src/main/java/com/study/PropertiesRunner.java
@@ -1,5 +1,7 @@
 package com.study;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
@@ -8,6 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PropertiesRunner implements ApplicationRunner {
+
+    private Logger logger = LoggerFactory.getLogger(PropertiesRunner.class);
 
     @Value("${name}")
     private String name;
@@ -23,11 +27,14 @@ public class PropertiesRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
+        logger.info("=======================");
         System.out.println("name : " + name);
         System.out.println("age : " + age);
         System.out.println("test name : " + testProperties.getName());
         System.out.println("test age : " + testProperties.getAge());
         System.out.println("test second : " + testProperties.getSecond());
         System.out.println("hello : " + hello);
+        logger.info("hello : {}", hello);
+        logger.info("=======================");
     }
 }
