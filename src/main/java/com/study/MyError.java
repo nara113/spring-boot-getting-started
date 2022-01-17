086f8368b11910baecffd09ef020b5c263b344b4
sedovila <0554my@naver.com>
2022-01-18 00:05:20 +0900

    ExceptionHandler

---
+++ src/main/java/com/study/MyError.java
@@ -0,0 +1,10 @@
+package com.study;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class MyError {
+    private String errorName;
+}
--- src/main/java/com/study/ThymeleafController.java
+++ src/main/java/com/study/ThymeleafController.java
@@ -2,14 +2,28 @@ package com.study;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ThymeleafController {
 
-    @GetMapping("name")
+    @GetMapping("/name")
     public String getHello(Model model) {
         model.addAttribute("name", "me");
         return "hello";
+    }
+
+    @GetMapping("/error")
+    public String getError() {
+        throw new SampleException();
+    }
+
+    @ExceptionHandler(SampleException.class)
+    public @ResponseBody
+    MyError error(SampleException e) {
+        return new MyError("error");
     }
 }
